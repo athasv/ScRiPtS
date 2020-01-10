9177438066ddac807bfba5a56d4c7b9e828fc06f
athasv <arvanitidisa@cardiff.ac.uk>
2020-01-10 15:29:58 +0000

    A new comment

--- gitall.sh
+++ gitall.sh
@@ -2,6 +2,6 @@
 #kk="git log --source"
 #eval $kk
 #store_number=$kk
-daynTime=$( date '+%F_%H:%M:%S' )
+#daynTime=$( date '+%F_%H:%M:%S' )
 
-git add . && git commit -am \"${dayntine}\" && git push
+git add . && git commit -am "A new comment" && git push
